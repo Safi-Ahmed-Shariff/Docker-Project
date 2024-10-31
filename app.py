e64287f6ee1bc907e9101afc44ce9111c1e79780
Safi-Ahmed-Shariff <safiahmedshariff@gmail.com>
2024-10-31 14:14:36 +0530

    Made necessary changes for the age calculator app to run

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello from Flask inside Docker!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    age = None
+    if request.method == 'POST':
+        birth_year = int(request.form['birth_year'])
+        age = 2024 - birth_year  # Calculate age
+    return render_template('index.html', age=age)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
